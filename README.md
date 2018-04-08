8f415b218846c154d2f44936e0094b9e3c983c75
smallclover <18363998103@163.com>
2018-04-08 20:17:15 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# SearchByBaidu
+
+Intellij IDEA 插件，选中文本，使用Baidu搜索。
+
+## 如何安装
+
+- 可以直接下载SearchByBaudu.jar 然后导入插件。
+- 或者在Intellij IDEA 插件仓库搜索 SearchByBaidu 来进行安装
+
+## 如何使用
+！[](use.gif)
+
+## LICENSE
+
+MIT LICENSE
---
+++ src/com/amadeus/action/SearchByBaidu.java
@@ -0,0 +1,38 @@
+package com.amadeus.action;
+
+import com.intellij.ide.BrowserUtil;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.CaretModel;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.swing.*;
+
+public class SearchByBaidu extends AnAction {
+
+    public SearchByBaidu(){
+        super(null, null, new ImageIcon(
+                SearchByBaidu.class.getClassLoader().getResource("icon/icon.png")
+        ));
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        String baidu = "https://www.baidu.com/s?wd=";
+        CaretModel caretModel = e.getData(LangDataKeys.EDITOR).getCaretModel();
+        Caret currentCaret = caretModel.getCurrentCaret();
+        String selectedText = currentCaret.getSelectedText().trim();
+        try{
+            selectedText = selectedText.replaceAll(" ", "+");
+        }catch (NullPointerException ex){
+            System.out.println("Text can't be empty" + ex.getMessage());
+        }
+        if (StringUtils.isNotBlank(selectedText)){
+            BrowserUtil.browse(baidu + selectedText);
+        }else {
+            BrowserUtil.browse(baidu);
+        }
+    }
+}
